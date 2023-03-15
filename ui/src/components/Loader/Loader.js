1e55014f0aa273fe6781c061cbdcb760b0e3eee7
vishalkrishnads <321vishalds@gmail.com>
2023-03-15 22:21:56 +0530

    Added a loading animation

---
+++ ui/src/components/Loader/Loader.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './styles.css'
+
+const Loader = () => {
+    return <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+}
+
+export default Loader;
