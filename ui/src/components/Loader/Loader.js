ca8c357aa62cad09e4e84c14b24f1a64649cd26a
vishalkrishnads <321vishalds@gmail.com>
2023-03-15 22:58:22 +0530

    Fixed a bug

--- ui/src/components/Loader/Loader.js
+++ ui/src/components/Loader/Loader.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './styles.css'
 
 const Loader = () => {
-    return <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+    return <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
 }
 
 export default Loader;
