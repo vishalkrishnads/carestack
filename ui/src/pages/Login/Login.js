5557f5317786bfe7d7a73f3a624f5b4c5d40c90a
vishalkrishnads <321vishalds@gmail.com>
2023-03-15 22:06:19 +0530

    Finished Login UI

--- ui/src/pages/Login/Login.js
+++ ui/src/pages/Login/Login.js
@@ -1,9 +1,24 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
+import Button from '../../components/Button/Button'
+import Field from '../../components/Field/Field'
+import './styles.css'
 
 const Login = () => {
-    return <div>
-        <h1>Login</h1>
+    return <div className='login'>
+    <div style={{ flex: 2, width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }} >
+        <h1>Hi there</h1>
+        <h2>Login to view your profile</h2>
     </div>
+    <div style={{ flex: 2, width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }} >
+        <Field placeholder={'Enter username or email'} />
+        <Field placeholder={'Enter password'} />
+    </div>
+    <div style={{ flex: 2, width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
+        <Button label={'Login'} />
+        <p>Don't have an account? <Link to="/signup" >Sign Up</Link></p>
+    </div>
+</div>
 }
 
 export default Login;
